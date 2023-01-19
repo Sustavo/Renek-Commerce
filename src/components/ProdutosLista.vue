2af9a1e1f23e4812bc85aa2e2cddaeb58f477afa
Gustavo <gust4v0309@gmail.com>
2023-01-19 19:41:58 -0300

    Feat: ProdutosLista and create 'getProdutos'

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="produtos-container">
+    <div >
+        
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    nome: "ProdutosLista",
+    data() {
+        return {
+            produtos: null,
+        }
+    },
+    methods: {
+        getProdutos() {
+            fetch("http://localhost:3000/produto")
+            .then(response => response.json())
+            .then(response => {
+                this.produtos = response;
+            })
+        }
+    },
+    created() {
+        this.getProdutos()
+    }
+}
+</script>
+
+<style>
+
+</style>
