6d2372818d1fbed8a391dea4d3993245d98a9c0a
Gustavo <gust4v0309@gmail.com>
2023-01-24 19:14:25 -0300

    Feat: create ProdutosBuscar and buscarProdutos function

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form action="">
+            <input name="busca" id="busca" type="text" v-model="busca">
+            <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            busca: ""
+        }
+    },
+    methods: {
+        buscarProdutos() {
+            this.$router.push({ query: { q: this.busca } })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
