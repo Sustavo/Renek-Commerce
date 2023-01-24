07a27c6c12479fe2f186f7d61f929b1da3781ff3
Gustavo <gust4v0309@gmail.com>
2023-01-24 20:08:39 -0300

    Feat: Adding pagination in Home.vue from "produtosPaginar"

---
+++ src/components/ProdutosPaginar.vue
@@ -0,0 +1,42 @@
+<template>
+        <ul v-if="paginasTotal > 1">
+            <li v-for="pagina in paginasTotal" :key="pagina">
+                <router-link :to="{query: query(pagina)}">{{ pagina }}</router-link>
+            </li>
+
+        </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        produtosPorPagina: {
+            type: Number,
+            default: 1,
+        },
+        produtosTotal: {
+            type: Number,
+            default: 1,
+        }
+    },
+    methods: {
+        query(pagina) {
+            return {
+                ...this.$route.query,
+                _page: pagina
+            }
+        }
+    },
+    computed: {
+        paginasTotal() {
+            const total = this.produtosTotal / this.produtosPorPagina;
+            return (total !== Infinity) ? Math.ceil(total) : 0;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
