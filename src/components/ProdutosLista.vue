c6dcd9707a11166f513f9e12f6b6e0db46a05160
Gustavo <gust4v0309@gmail.com>
2023-01-25 16:03:53 -0300

    Feat: adding transition in "ProdutosLista"

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,19 +1,23 @@
 <template>
   <section class="produtos-container">
-    <div v-if="produtos && produtos.length" class="produtos" >
-        <div class="produto" v-for="(produto, index) in produtos" :key="index">
-            <router-link to="/">
-                <img v-if="produto.fotos" src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
-                <p class="preco">{{ produto.preco }}</p>
-                <h2 class="titulo">{{ produto.nome }}</h2>
-                <p>{{ produto.descricao }}</p>
-            </router-link>
+    <transition mode="out-in">
+
+        <div v-if="produtos && produtos.length" class="produtos" key="produtos">
+            <div class="produto" v-for="(produto, index) in produtos" :key="index">
+                <router-link to="/">
+                    <img v-if="produto.fotos" src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
+                    <p class="preco">{{ produto.preco }}</p>
+                    <h2 class="titulo">{{ produto.nome }}</h2>
+                    <p>{{ produto.descricao }}</p>
+                </router-link>
+            </div>
+            <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina" />
         </div>
-        <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina" />
-    </div>
-    <div v-else-if="produtos && produtos.length === 0" class="sem-resultados">
-        <p>Busca sem resultados. Tente buscar outro termo </p>
-    </div>
+        <div v-else-if="produtos && produtos.length === 0" class="sem-resultados" key="sem-resultados">
+            <p>Busca sem resultados. Tente buscar outro termo </p>
+        </div>
+        <PaginaCarregando v-else key="carregando"/>
+    </transition>
   </section>
 </template>
 
@@ -42,6 +46,7 @@ export default {
     },
     methods: {
         getProdutos() {
+            this.produtos = null;
             api.get(this.url)
             .then(response => {
                 this.produtosTotal = Number(response.headers['x-total-count']);
