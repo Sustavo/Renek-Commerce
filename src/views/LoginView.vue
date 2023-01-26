9ba7a41e0e8b5e122df224eff5e70b0045f2fe67
Gustavo <gust4v0309@gmail.com>
2023-01-26 00:07:10 -0300

    Feat: Create Template to the 'LoginView'

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="login">
+    <h1>Login</h1>
+    <form action="">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" v-model="login.email">
+        <label for="senha">Senha</label>
+        <input type="password" name="senha" id="senha" v-model="login.senha">
+        <button class="btn" @click.prevent="logar">Logar</button>
+    </form>
+    <p class="perdeu">
+        <a href="/" target="_blank">Perdeu a senha? Clique aqui.</a>
+    </p>
+    <LoginCriar />
+  </section>
+</template>
+
+<script>
+import LoginCriar from "@/components/LoginCriar.vue"
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
