9f448752f2ae5ec3776083c06de950834dc674ad
Gustavo <gust4v0309@gmail.com>
2023-01-25 23:50:00 -0300

    Feat: Create function 'getProduto' in ProdutoView

--- src/views/ProdutoView.vue
+++ src/views/ProdutoView.vue
@@ -19,12 +19,28 @@
 </template>
 
 <script>
+import { api } from '@/services.js'
 
 
 export default {
     name: "ProdutoView",
     props: ["id"],
-
+    data() {
+      return {
+        produto: null
+      }
+    },
+    methods: {
+      getProduto() {
+        api.get(`/produto/${this.id}`)
+        .then(response => {
+          this.produto = response.data
+        })
+      }
+    },
+    created() {
+      this.getProduto();
+    }
 }
 </script>
 
