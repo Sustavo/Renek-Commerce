f74ae2d4fad65ce444961106733d5e21b823fd8d
Gustavo <gust4v0309@gmail.com>
2023-01-26 00:08:06 -0300

    Feat: create scope to function 'logar' and styling LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -19,10 +19,58 @@
 import LoginCriar from "@/components/LoginCriar.vue"
 
 export default {
+    name: "LoginView",
+    components: {
+        LoginCriar
+    },
+    data() {
+        return {
+            login: {
+                email: "",
+                senha: ""
+            }
+        }
+    },
+    methods: {
+        logar() {
 
+        }
+    }
 }
 </script>
 
 <style scoped>
+    .login {
+        max-width: 500px;
+        margin: 0 auto;
+        padding: 0 20px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 40px;
+        color: #87f;
+    }
+
+    form {
+        display: grid;
+    }
+
+    .btn {
+        width: 100%;
+        max-width: 300px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .perdeu {
+        text-align: center;
+        margin: 20px auto 0 auto;
+    }
+
+    .perdeu a:hover{
+        color: #87f;
+    }
 
 </style>
