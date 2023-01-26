d8866f6fbf33e13b1630256390f75cd45d2a5d76
Gustavo <gust4v0309@gmail.com>
2023-01-25 23:49:32 -0300

    Feat: Create "ProdutoView" to of the unique view the product

---
+++ src/views/ProdutoView.vue
@@ -0,0 +1,34 @@
+<template>
+  <section >
+    <div v-if="produto" class="produto">
+      <ul class="fotos" v-if="produto.fotos">
+        <li v-for="(foto, index) in produtos.fotos" :key="index">
+          <img :src="foto.src" :alt="foto.titulo" >
+        </li>
+      </ul>
+      <div class="info">
+        <h1>{{ produto.nome }}</h1>
+        <p class="preco">{{ produto.preco | numeroPreco }}</p>
+        <p class="descricao">{{ produto.descricao }}</p>
+        <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
+        <button class="btn" disabled v-else>Produto Vendido</button>
+      </div>
+    </div>
+    <PaginaCarregando v-else/>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+    name: "ProdutoView",
+    props: ["id"],
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
