5e339bfc9ef2674f098c36c3d3c73616095a8395
Gustavo <gust4v0309@gmail.com>
2023-01-27 02:30:27 -0300

    Feat: create states login and usuario

--- src/store.js
+++ src/store.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { api } from "./services";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        login: false,
+        usuario: {
+            id: "",
+            nome: "",
+            email: "",
+            senha: "",
+            cep: "",
+            rua: "",
+            numero: "",
+            bairro: "",
+            cidade: "",
+            estado: ""
+        }
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
 })
