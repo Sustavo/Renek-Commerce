156fe7ab36345f27ccb57568a8f0ff3a62e21f7f
Gustavo <gust4v0309@gmail.com>
2023-01-27 03:07:22 -0300

    Feat: Create Template and import components to "LoginCriar"

---
+++ src/components/LoginCriar.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>Crie a Sua Conta</h2>
+    <transition mode="out-in">
+      <button v-if="!criar" class="btn " @click="criar = true">Criar Conta</button>
+      <UsuarioForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UsuarioForm>
+    </transition>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from "@/components/UsuarioForm.vue"
+
+export default {
+    name: "LoginCriar",
+    components: {
+      UsuarioForm
+    },
+    data() {
+      return {
+        criar: false,
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
