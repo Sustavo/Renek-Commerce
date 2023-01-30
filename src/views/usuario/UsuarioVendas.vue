77c25599692312af67d3b389282d34d833870190
Gustavo <gust4v0309@gmail.com>
2023-01-30 20:44:49 -0300

    Feat: Make Template and Import "ProdutoItem" in "UsuarioVendas"

---
+++ src/views/usuario/UsuarioVendas.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div class="produtos-wrapper" v-for="(venda, index) in vendas" :key="index">
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor">
+            <span>Comprador:</span>
+            {{ venda.comprador_id }}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega: </h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value,key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProdutoItem from '@/components/ProdutoItem.vue';
+import { api } from '@/services';
+
+export default {
+  components: {
+    ProdutoItem
+  },
+  
+
+}
+</script>
+
+<style scoped>
+
+</style>
