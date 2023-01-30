0318e477c60fc716890ed17b4703126e0aab8ed1
Gustavo <gust4v0309@gmail.com>
2023-01-29 22:00:55 -0300

    Feat: Create template and import "UsuarioForm" to the "UsuarioEditar"

---
+++ src/views/usuario/UsuarioEditar.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">Atualizar Usuário</button>
+    </UsuarioForm>
+  </section>
+</template>
+
+<script>
+import UsuarioForm from '@/components/UsuarioForm.vue';
+
+export default {
+  name: "UsuarioEditar",
+  components: {
+    UsuarioForm
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
