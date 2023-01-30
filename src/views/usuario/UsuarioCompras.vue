08c3f82a573a2b3c67febda08b0d01bcb66ea2df
Gustavo <gust4v0309@gmail.com>
2023-01-30 20:33:34 -0300

    feat: import ProdutoItem and create template UsuarioCompras

---
+++ src/views/usuario/UsuarioCompras.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div class="produtos-wrapper" v-for="(compra, index) in compras" :key="index">
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor">
+            <span>Vendedor:</span>
+            {{ compra.vendedor_id }}
+          </p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ProdutoItem from '@/components/ProdutoItem.vue';
+import { api } from '@/services';
+
+export default {
+  components: {
+    ProdutoItem
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
