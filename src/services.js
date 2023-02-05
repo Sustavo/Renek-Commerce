c5604e518197150e3078c54c9381833067f146c7
Gustavo <gust4v0309@gmail.com>
2023-02-05 19:15:57 -0300

    Feat: Create a authorization to login in the website

--- src/services.js
+++ src/services.js
@@ -4,6 +4,19 @@ const axiosInstance = axios.create({
     baseURL: "http://localhost:10005/wp-json/api"
 })
 
+axiosInstance.interceptors.request.use(
+    function(config) {
+        const token = window.localStorage.token;
+        if(token) {
+            config.headers.Authorization = token 
+        }
+        return config;
+    },
+    function(error) {
+        return Promise.reject(error);
+    }
+);
+
 export const api = {
     get(endpoint) {
         return axiosInstance.get(endpoint)
