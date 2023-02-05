79d45a343f273d770949c57d445cb999c078d67a
Gustavo <gust4v0309@gmail.com>
2023-02-05 20:55:25 -0300

    Feat: Create 'ErroNotificacao" to warn this errors

---
+++ src/components/ErroNotificacao.vue
@@ -0,0 +1,19 @@
+<template>
+  <transition>
+    <ul class="erro" v-if="erros.length > 0">
+        <li v-for="(erro, index) in erros" :key="index" v-html="erro"></li>
+    </ul>
+  </transition>
+</template>
+
+<script>
+export default {
+    name: "ErroNotificacao",
+    props: {
+        erros: Array
+    }
+}
+</script>
+
+<style scoped>  
+</style>
