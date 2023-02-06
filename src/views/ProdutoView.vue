2bf9321fd20416f4fbbbc0d53893b6f6ef07c281
Gustavo <gust4v0309@gmail.com>
2023-02-05 21:48:22 -0300

    Style: add @media screen in "ProdutoView" to make responsive

--- src/views/ProdutoView.vue
+++ src/views/ProdutoView.vue
@@ -93,4 +93,21 @@ export default {
     margin-top: 60px;
     width: 200px;
   }
+
+  @media screen and (max-width: 500px) {
+  .produto {
+      grid-template-columns: 1fr;
+   }
+
+   .fotos {
+      grid-row: 2;
+      
+   }
+
+   .info {
+      position: initial;
+
+   }
+}
+
 </style>
